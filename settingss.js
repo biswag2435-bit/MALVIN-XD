ebc9247031f95890009071ebf6829f9d3ec53c85
biswag2435-bit <biswag2435@gmail.com>
2025-08-22 01:34:10 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: ""malvin~Ej9SBSyJ#HwfqpNJAruiFueD9DZjyMqypzO-LCN6CKHd5WWIUhZw,  // add your Session Id here
+  SESSION_ID: ""malvin~YjkFVSIb#UVc_kF-ZtENDDDyqqgCVoPAeJPnliQLutESCgqNisBM,  // add your Session Id here
   
   OWNER_NUMBER: "+91 81016 86625", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Harare"24hours //put your country timeZone....leave blank if u don't know.
 };
