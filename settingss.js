70f43ee09f993be4305f2d9e3d3fee5bc4796eff
biswag2435-bit <biswag2435@gmail.com>
2025-08-22 00:52:55 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: ""malvin~Ej9SBSyJ#HwfqpNJAruiFueD9DZjyMqypzO-LCN6CKHd5WWIUhZw,  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
